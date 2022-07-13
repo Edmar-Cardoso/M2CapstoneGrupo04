94e1038c7b067ec07852a77a931b810b21d82a86
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-07-13 09:01:14 -0300

    feat: editarHabitoModal

--- src/Js/scripts/homePage.script.js
+++ src/Js/scripts/homePage.script.js
@@ -1,7 +1,9 @@
 
 import { MontandoModalCriarTarefa } from "../controllers/montandoModalCriarTarefa.controller.js";
 import dadosUsuario from "../controllers/dadosUsuario.controller.js";
+import editarHabitoModal from "../controllers/modalEditarHabito.controller.js";
 
 MontandoModalCriarTarefa.montandoDomModal()
 await dadosUsuario.pegarDadosUsuario()
-
+editarHabitoModal.editarModal()
+editarHabitoModal.editarHabito()
