213d578edd1307d64837917b45ed0ef1d70549df
Paulo Gustavo <arknum@hotmail.com>
2022-07-13 19:14:08 -0300

    feat:CssQuaseLa

--- src/Js/models/habitos.model.js
+++ src/Js/models/habitos.model.js
@@ -13,19 +13,34 @@ listagemHabitos(){
 
     const li               = document.createElement("li");
 
-    const Status           = document.createElement("p");
+    const Status           = document.createElement("input");
+    Status.type = "checkbox"
+    Status.id = "myCheck"
+    Status.value = `${this.habit_status}`
+
+
     const TituloHabito     = document.createElement("p");
     const DescricaoHabito  = document.createElement("p");
-    const Categoriahabito  = document.createElement("p");
+    const Categoriahabito  = document.createElement("button");
     const EditarHabito     = document.createElement("button");
     
+
+    Status.addEventListener("click", () => {
+        if(this.habit_status === true){
+            this.habit_status = false
+        }else {
+            this.habit_status = true
+        }
+
+        console.log(this.habit_status)
+    })
 
 
     Status.innerText           = this.habit_status;
     TituloHabito.innerText     = this.habit_title;
     DescricaoHabito.innerText  = this.habit_description;
-    Categoriahabito.innerText  = this.habit_category;
-    EditarHabito.innerText     = "...";
+    Categoriahabito.innerText  = this.habit_category[0].toUpperCase() + this.habit_category.substr(1) ;
+    EditarHabito.innerText     = ". . .";
 
     Status.classList.add("status2");
     TituloHabito.classList.add("titulo");
