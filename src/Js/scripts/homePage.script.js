c722e00c0eb9a01c8816de8adab204063e08bcde
Enrique <enrique.barbosasilva@gmail.com>
2022-07-13 12:26:51 -0300

    Feat: novas funcionalidades implementadas e filtro de todos pronto

--- src/Js/scripts/homePage.script.js
+++ src/Js/scripts/homePage.script.js
@@ -3,10 +3,10 @@ import { MontandoModalCriarTarefa } from "../controllers/montandoModalCriarTaref
 import dadosUsuario from "../controllers/dadosUsuario.controller.js";
 import Requisicao from "../controllers/habitosAPI.controller.js";
 import HabitosApi from "../controllers/listarHabitos.controller.js";
+import FiltrarTodos from "../controllers/filtros.controller.js";
 
+
+await FiltrarTodos.listagemDeTodos()
 await dadosUsuario.pegarDadosUsuario()
 await HabitosApi.listarHabitos()
 MontandoModalCriarTarefa.montandoDomModal()
-
-
-
